3cfac29712c5a14ef0a36555017786d402161c92
Vibhav Tomar <52465652+reapedjuggler@users.noreply.github.com>
2020-12-20 17:13:29 +0530

    Update product.js

--- routes/api/product.js
+++ routes/api/product.js
@@ -49,10 +49,3 @@ exports = module.exports = {
 // I learned express.js a framework of Node.js and also i got knowledge of sequelize library
 
 
-
-
-// Ever since i learned programming i found it very fascinating and so i learned Data Structures and Algorithms, and few months back i started contributing to Open Source and i found it very interesting and thus i want to learn more and contribute more towards the community.
-
-// I recently learned JavaScript and then i learned Node.js, also Express.js ( A framework of Node.js) and i also learned about API's and REST api's also i enhanced my knowledge on Data Base Management Systems, 
-
-// I'am an avid learner and also a very hard working and motivated person and my curiosity never ends, thus i want to delve around more interesting tech stack and learn more to solve real world problems. 
